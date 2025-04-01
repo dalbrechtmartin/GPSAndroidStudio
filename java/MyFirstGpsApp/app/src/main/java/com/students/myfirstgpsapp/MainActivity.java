3476d54a354d16211fde32b224c4627df8b8c156
Danaé A.M. <dev.danae@gmail.com>
2025-04-01 11:35:01 +0200

    GPS

--- java/MyFirstGpsApp/app/src/main/java/com/students/myfirstgpsapp/MainActivity.java
+++ java/MyFirstGpsApp/app/src/main/java/com/students/myfirstgpsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.students.myfirstgpsapp;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -22,8 +24,12 @@ import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.material.navigation.NavigationView;
@@ -66,8 +72,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         m_distanceParcourueTv = findViewById(R.id.distanceParcourue);
 
         // TODO 10) use SupportMapFragment on layout fragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         // TODO 13) get locationManager
+        m_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         // 6 - Configure all views
 
@@ -87,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         // TODO 14) use locationManager to get notified of location Updates
+        m_locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5, this);
     }
 
     @Override
@@ -99,7 +110,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    // TODO 8) override method onNavigationItemSelected
+    // TODO 7) override method onNavigationItemSelected
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.activity_main_start_new_path) {
+            // code ici
+        } else if (id == R.id.activity_main_exit) {
+            finish();
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
     // TODO 16) call method askPathNameBeforeCreating on click start new path
 
     private void askPathNameBeforeCreating() {
@@ -115,16 +140,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     // 1 - Configure Toolbar
     private void configureToolBar() {
         // TODO 1) fill this method to link toolbar to Activity
+        this.toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     // 2 - Configure Drawer Layout
     private void configureDrawerLayout() {
         // TODO 2) fill this method to link menu to the toolbar
+        this.drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     // 3 - Configure NavigationView
     private void configureNavigationView() {
-        // TODO 3) fill this method to be notified of navigation menu item click
+        // TODO 4) fill this method to be notified of navigation menu item click
+        this.navigationView = findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -158,7 +192,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        Log.v("MainActivity", ">onLocationChanged : Latituude " + location.getLatitude() + "et longitude " + location.getLongitude());
 
+        if (!m_newPathStarted) return;
+        // Add a marker in current position and move the camera
+        LatLng myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        if (m_currentPath != null) {
+            GpsPoint currentPoint = new GpsPoint(location.getLatitude(), location.getLongitude());
+            m_currentPath.addGpsPoint(currentPoint);
+            m_gpsPathDbHelper.addPathGpsPoint(m_currentPath.getGpsPointTableName(), currentPoint);
+        }
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(myPosition);
+        if (m_myLastLocation == null) {
+            markerOptions.title("Starting Point");
+        } else {
+            m_distanceParcourue += (int) m_myLastLocation.distanceTo(location);
+            m_distanceParcourueTv.setText(String.valueOf(m_distanceParcourue));
+        }
+        m_myLastLocation = location;
+        m_googleMap.addMarker(markerOptions);
+        m_googleMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
     }
 
     @Override
@@ -172,13 +226,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+    public void onMapReady(GoogleMap googleMap) {
+        Log.v(LOG_TAG, ">onMapReady");
+        m_googleMap = googleMap;
+        // Add a marker in Sydney and move the camera
+         LatLng sydney = new LatLng(-34, 151);
+         m_googleMap.addMarker(new MarkerOptions()
+         .position(sydney)
+         .title("Marker in Sydney"));
+         m_googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     // TODO 11) override onMapReady and save given googleMap
